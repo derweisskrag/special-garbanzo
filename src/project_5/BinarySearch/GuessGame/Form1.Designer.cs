c6f75ce584f504014962ae00eb59500dc491ea99
Sergei Ivanov <kompasj@mail.ru>
2024-10-24 14:04:46 +0300

    Adding the project 5 with tests

---
+++ src/project_5/BinarySearch/GuessGame/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace GuessGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GuessContainer = new System.Windows.Forms.GroupBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.GuessLabel = new System.Windows.Forms.Label();
+            this.UserInput = new System.Windows.Forms.TextBox();
+            this.ConfirmButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.AnalyticsContainer = new System.Windows.Forms.GroupBox();
+            this.Results = new System.Windows.Forms.RichTextBox();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.Label();
+            this.GiveUpButton = new System.Windows.Forms.Button();
+            this.ComputerGuessResult = new System.Windows.Forms.Label();
+            this.ComputerGuessResultValue = new System.Windows.Forms.Label();
+            this.GuessContainer.SuspendLayout();
+            this.AnalyticsContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GuessContainer
+            // 
+            this.GuessContainer.Controls.Add(this.ComputerGuessResultValue);
+            this.GuessContainer.Controls.Add(this.ComputerGuessResult);
+            this.GuessContainer.Controls.Add(this.GiveUpButton);
+            this.GuessContainer.Controls.Add(this.Status);
+            this.GuessContainer.Controls.Add(this.StartButton);
+            this.GuessContainer.Controls.Add(this.GuessLabel);
+            this.GuessContainer.Controls.Add(this.UserInput);
+            this.GuessContainer.Controls.Add(this.ConfirmButton);
+            this.GuessContainer.Location = new System.Drawing.Point(12, 12);
+            this.GuessContainer.Name = "GuessContainer";
+            this.GuessContainer.Size = new System.Drawing.Size(212, 175);
+            this.GuessContainer.TabIndex = 0;
+            this.GuessContainer.TabStop = false;
+            this.GuessContainer.Text = "Guess Number";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(18, 70);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 3;
+            this.StartButton.Text = "START";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // GuessLabel
+            // 
+            this.GuessLabel.AutoSize = true;
+            this.GuessLabel.Location = new System.Drawing.Point(15, 37);
+            this.GuessLabel.Name = "GuessLabel";
+            this.GuessLabel.Size = new System.Drawing.Size(60, 13);
+            this.GuessLabel.TabIndex = 2;
+            this.GuessLabel.Text = "Your guess";
+            // 
+            // UserInput
+            // 
+            this.UserInput.Location = new System.Drawing.Point(94, 34);
+            this.UserInput.Name = "UserInput";
+            this.UserInput.Size = new System.Drawing.Size(100, 20);
+            this.UserInput.TabIndex = 1;
+            // 
+            // ConfirmButton
+            // 
+            this.ConfirmButton.Location = new System.Drawing.Point(119, 71);
+            this.ConfirmButton.Name = "ConfirmButton";
+            this.ConfirmButton.Size = new System.Drawing.Size(75, 23);
+            this.ConfirmButton.TabIndex = 0;
+            this.ConfirmButton.Text = "Confirm";
+            this.ConfirmButton.UseVisualStyleBackColor = true;
+            this.ConfirmButton.Click += new System.EventHandler(this.ConfirmButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(637, 369);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(151, 69);
+            this.ExitButton.TabIndex = 1;
+            this.ExitButton.Text = "EXIT";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ExitButton_MouseClick);
+            // 
+            // AnalyticsContainer
+            // 
+            this.AnalyticsContainer.Controls.Add(this.Results);
+            this.AnalyticsContainer.Location = new System.Drawing.Point(588, 12);
+            this.AnalyticsContainer.Name = "AnalyticsContainer";
+            this.AnalyticsContainer.Size = new System.Drawing.Size(200, 269);
+            this.AnalyticsContainer.TabIndex = 2;
+            this.AnalyticsContainer.TabStop = false;
+            this.AnalyticsContainer.Text = "Analytics";
+            // 
+            // Results
+            // 
+            this.Results.Location = new System.Drawing.Point(6, 34);
+            this.Results.Name = "Results";
+            this.Results.Size = new System.Drawing.Size(188, 229);
+            this.Results.TabIndex = 0;
+            this.Results.Text = "";
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(488, 369);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(143, 69);
+            this.ResetButton.TabIndex = 3;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(362, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 90);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Does Nothing You Can chill";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Location = new System.Drawing.Point(15, 112);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(81, 13);
+            this.Status.TabIndex = 5;
+            this.Status.Text = "Status: Inactive";
+            // 
+            // GiveUpButton
+            // 
+            this.GiveUpButton.Location = new System.Drawing.Point(119, 107);
+            this.GiveUpButton.Name = "GiveUpButton";
+            this.GiveUpButton.Size = new System.Drawing.Size(75, 23);
+            this.GiveUpButton.TabIndex = 5;
+            this.GiveUpButton.Text = "Give Up";
+            this.GiveUpButton.UseVisualStyleBackColor = true;
+            this.GiveUpButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.GiveUpButton_MouseClick);
+            // 
+            // ComputerGuessResult
+            // 
+            this.ComputerGuessResult.AutoSize = true;
+            this.ComputerGuessResult.Location = new System.Drawing.Point(15, 147);
+            this.ComputerGuessResult.Name = "ComputerGuessResult";
+            this.ComputerGuessResult.Size = new System.Drawing.Size(91, 13);
+            this.ComputerGuessResult.TabIndex = 6;
+            this.ComputerGuessResult.Text = "Computer Guess: ";
+            // 
+            // ComputerGuessResultValue
+            // 
+            this.ComputerGuessResultValue.AutoSize = true;
+            this.ComputerGuessResultValue.Location = new System.Drawing.Point(119, 147);
+            this.ComputerGuessResultValue.Name = "ComputerGuessResultValue";
+            this.ComputerGuessResultValue.Size = new System.Drawing.Size(0, 13);
+            this.ComputerGuessResultValue.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.AnalyticsContainer);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.GuessContainer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.GuessContainer.ResumeLayout(false);
+            this.GuessContainer.PerformLayout();
+            this.AnalyticsContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GuessContainer;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label GuessLabel;
+        private System.Windows.Forms.TextBox UserInput;
+        private System.Windows.Forms.Button ConfirmButton;
+        private System.Windows.Forms.GroupBox AnalyticsContainer;
+        private System.Windows.Forms.RichTextBox Results;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.Button GiveUpButton;
+        private System.Windows.Forms.Label ComputerGuessResult;
+        private System.Windows.Forms.Label ComputerGuessResultValue;
+    }
+}
+
---
+++ src/project_5/BinarySearch/GuessGame/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessGame
+{   
+    // Enum for options (low or high)
+    enum GuessOption
+    {
+        High,
+        Low,
+        Match,
+        ErrorGuess
+    }
+
+    public partial class Form1 : Form
+    {
+        // later I will use SQLite. Now I try this as my storage
+        private Dictionary<int, GuessOption> GuessTable = new Dictionary<int, GuessOption>();
+        private int ComputerGuess = 0;
+        private int attempts = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Guess Game";
+        }
+
+        private void ExitButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("Action: Exit", "Confirm Exit", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.OK)
+                {
+                    this.Close();
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    MessageBox.Show("Exit Canceled");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you play?", "Yes", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            switch (result) 
+            { 
+                case DialogResult.Yes:
+                    this.StartGame();
+                    break;
+                case DialogResult.No:
+                    break;   
+            }
+
+        }
+
+        private void StartGame()
+        {
+            // create RNG 
+            // Store the guess
+            Random random = new Random();
+            int ComputerGuess = random.Next(1, 101);
+            
+            // Store it
+            this.ComputerGuess = ComputerGuess;
+            this.Status.Text = "Active";
+        }
+
+        private int CompareGuess(int a, int b)
+        {
+            return a > b ? 1 : a < b ? -1 : 0;
+        }
+
+        private void ConfirmButton_Click(object sender, EventArgs e)
+        {
+            // user tries to guess
+            // write classic binary search
+            int UserGuess = int.Parse(this.UserInput.Text);
+            
+            // compare the guess
+            int ComparedResult = this.CompareGuess(this.ComputerGuess, UserGuess);
+
+            // store
+            switch (ComparedResult)
+            {
+                case 1:
+                    this.GuessTable.Add(UserGuess, GuessOption.High); break;
+                case -1:
+                    this.GuessTable.Add(UserGuess, GuessOption.Low); break;
+                case 0:
+                    this.GuessTable.Add(UserGuess, GuessOption.Match); break;
+                default:
+                    this.GuessTable.Add(UserGuess, GuessOption.ErrorGuess); break;
+            }
+
+            if(ComparedResult == 0)
+            {
+                DialogResult result = MessageBox.Show("You guessed correctly!\nNow you can reset the game and start over again!", "Confirm", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                if(result == DialogResult.OK)
+                {
+                    this.Status.Text = "Finished! Start over again!";
+                }
+
+            } else
+            {
+                string hint = ComparedResult == 1 ? "higher" : "lower";
+                MessageBox.Show($"Your guess is wrong! Try guessing {hint}.", "Confirm", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+
+            // update the log
+            this.UpdateDisplay(UserGuess);
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("Do you reset the game?", "Confirm Reset", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.OK)
+                {
+                    this.Reset();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // nothing
+            // Did you enjoy this button?
+        }
+
+        private void DisplayButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            // display
+        }
+
+        private void Reset()
+        {
+            this.GuessTable.Clear();
+        }
+
+        private void UpdateDisplay(int UserGuess)
+        {
+            try
+            {
+                GuessOption result = this.GuessTable[UserGuess];
+                this.Results.Text += $"\nattempt {this.attempts}\n\tResult: {result}";
+                this.attempts++;
+            }catch(Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void GiveUpButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you give up?", "Confirm Give Up", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (result == DialogResult.OK)
+            {
+                this.Status.Text = "Gave up! Inactive";
+                this.ComputerGuessResultValue.Text = $"{this.ComputerGuess}";
+                MessageBox.Show($"Computer Guess was {this.ComputerGuess}");
+            }
+        }
+
+        public int ExampleMethod(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
